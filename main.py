11f8a754c52ae928ac63a60f0f5305acd1ead0cd
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-06-24 22:12:34 -0300

    feat: add controller

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from src.controllers.knowledge_base import router as knowledge_base_router
+
+app = FastAPI(
+    title="Knowly API",
+    description="API para gerenciamento de bases de conhecimento e usuários.",
+    version="1.0.0",
+)
+
+# Knowledge Bases
+app.include_router(knowledge_base_router)
