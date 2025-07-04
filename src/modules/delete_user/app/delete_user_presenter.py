bcb414696601e7efb1f4a9b512b640d4be28fcf5
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-07-03 21:01:31 -0300

    feat: add DeleteUserPresenter and tests

--- src/modules/delete_user/app/delete_user_presenter.py
+++ src/modules/delete_user/app/delete_user_presenter.py
@@ -1,7 +1,7 @@
-from .delete_user_controller import DeleteUserController
-from .delete_user_usecase import DeleteUserUseCase
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from .delete_user_controller import DeleteUserController
+from .delete_user_usecase import DeleteUserUseCase
 
 repo = Environments.get_user_repo()()
 usecase = DeleteUserUseCase(repo)
@@ -9,8 +9,8 @@ controller = DeleteUserController(usecase)
 
 
 def lambda_handler(event, context):
-    httpRequest = LambdaHttpRequest(data=event)
-    response = controller(httpRequest)
-    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    http_request = LambdaHttpRequest(data=event)
+    response = controller(http_request)
+    http_response = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
-    return httpResponse.toDict()
+    return http_response.toDict()
