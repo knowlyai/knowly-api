62489ccd609382314ed7d29df67707a57390a29d
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-07-17 20:31:58 -0300

    feat: add all endpoints

--- src/controllers/knowledge_base.py
+++ src/controllers/knowledge_base.py
@@ -1,20 +1,39 @@
 from fastapi import APIRouter, HTTPException
+
+from src.modules.create_kb.app.create_kb_controller import CreateKbController
 from src.modules.get_presigned_bucket_url.app.get_presigned_bucket_url_controller import GetPresignedBucketUrlController
 from src.modules.get_presigned_bucket_url.app.get_presigned_bucket_url_usecase import GetPresignedBucketUrlUseCase
+from src.modules.create_kb.app.create_kb_usecase import CreateKbUseCase
+from src.modules.sync_kb.app.sync_kb_controller import SyncKbController
+from src.modules.sync_kb.app.sync_kb_usecase import SyncKbUseCase
+from src.modules.delete_kb_file.app.delete_kb_file_controller import DeleteKbFileController
+from src.modules.delete_kb_file.app.delete_kb_file_usecase import DeleteKbFileUseCase
 from src.shared.helpers.external_interfaces.http_models import HttpRequest
-from src.shared.helpers.functions.generate_presigned_bucket_url import generate_presigned_bucket_url
 
 router = APIRouter(prefix="/kb", tags=["Knowledge Bases"])
 
 
-@router.get("/upload-files", summary="Enviar arquivos para a base de conhecimento")
-async def upload_files(bucket: str, user_id: str, kb_id: str, expires: int = 900, max_size_mb: int = 20):
-    params = generate_presigned_bucket_url(
-        bucket=bucket,
-        prefix=f"{user_id}/{kb_id}/",
-        expires=expires,
-        max_size_mb=max_size_mb
-    )
+@router.post("", summary="Cria uma base de conhecimento na AWS")
+async def create_kb(kb_name: str, kb_description: str):
+    use_case = CreateKbUseCase()
+    controller = CreateKbController(use_case)
+    params = {
+        "kb_name": kb_name,
+        "kb_description": kb_description
+    }
+    request = HttpRequest(query_params=params)
+    response = controller(request)
+
+    if not response:
+        raise HTTPException(
+            status_code=500, detail="Algo deu errado ao criar a base de conhecimento"
+        )
+
+    return response.data
+
+
+@router.get("/url-presigned", summary="Gera URL Presigned para enviar arquivos para uma base de conhecimento")
+async def get_url_presigned(bucket: str, user_id: str, kb_id: str, expires: int = 900, max_size_mb: int = 20):
     use_case = GetPresignedBucketUrlUseCase()
     controller = GetPresignedBucketUrlController(use_case)
     params = {
@@ -29,7 +48,48 @@ async def upload_files(bucket: str, user_id: str, kb_id: str, expires: int = 900
 
     if not response:
         raise HTTPException(
-            status_code=500, detail="Erro ao gerar URL pré-assinada"
+            status_code=500, detail="Algo deu errado ao gerar URL pré-assinada"
+        )
+
+    return response.data
+
+
+@router.get("/sync", summary="Sincroniza uma base de conhecimento com os arquivos enviados para o S3")
+async def create_kb(bucket_name: str, user_id: str, kb_id: str):
+    use_case = SyncKbUseCase()
+    controller = SyncKbController(use_case)
+    params = {
+        "bucket_name": bucket_name,
+        "user_id": user_id,
+        "kb_id": kb_id
+    }
+    request = HttpRequest(query_params=params)
+    response = controller(request)
+
+    if not response:
+        raise HTTPException(
+            status_code=500, detail="Algo deu errado ao sincronizar a base de conhecimento"
+        )
+
+    return response.data
+
+
+@router.delete("/file", summary="Deleta um arquivo de uma base de conhecimento no S3")
+async def delete_kb_file(bucket: str, user_id: str, kb_id: str, file_name: str):
+    use_case = DeleteKbFileUseCase()
+    controller = DeleteKbFileController(use_case)
+    params = {
+        "bucket": bucket,
+        "user_id": user_id,
+        "kb_id": kb_id,
+        "file_name": file_name
+    }
+    request = HttpRequest(query_params=params)
+    response = controller(request)
+
+    if not response:
+        raise HTTPException(
+            status_code=500, detail="Algo deu errado ao deletar o arquivo da base de conhecimento"
         )
 
     return response.data
