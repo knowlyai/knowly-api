687e50fbba472eedd16da281a77b62da81ff8bda
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-07-28 21:57:54 -0300

    refactor: remove useless feature

--- src/features/knowledge_base/interfaces/get_knowledge_base_use_case_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import abstractmethod
-from src.shared.domain.entities.knowledge_base import KnowledgeBase
-
-
-class GetKnowledgeBaseInterface:
-    @abstractmethod
-    async def execute(self, knowledge_base_id: str) -> KnowledgeBase | None:
-        pass
