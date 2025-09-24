a2ee737c637d9d9d804c3c80e2250fd4e03fe931
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-09-24 19:31:48 -0300

    feat: add requester_user to http_request data in get_kb_presenter

--- src/modules/get_kb/app/get_kb_presenter.py
+++ src/modules/get_kb/app/get_kb_presenter.py
@@ -11,6 +11,7 @@ controller = GetKbController(use_case)
 
 def get_kb_presenter(event):
     http_request = LambdaHttpRequest(event)
+    http_request.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
     response = controller(http_request)
     http_response = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
     return http_response.toDict()
