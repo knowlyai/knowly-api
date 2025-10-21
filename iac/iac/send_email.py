dfb91d457929615f9005e6ac15a9b25407216b97
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-10-20 23:52:02 -0300

    feat: update email verification link in send_email.py

--- iac/iac/send_email.py
+++ iac/iac/send_email.py
@@ -2,7 +2,9 @@ def send_email_presenter(event, context):
 
     user = event['request']['userAttributes']
     name = user['name'].split(' ')[0]
-    code = event['request']['codeParameter']
+    link = event['request']['codeParameter']
+
+    print(event)
 
     if event['triggerSource'] == 'CustomMessage_SignUp' or event['triggerSource'] == "CustomMessage_ResendCode":
         message = """
@@ -286,7 +288,9 @@ def send_email_presenter(event, context):
         <tr>
         <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:15px;font-family:'Open Sans',sans-serif;" align="left">
         <div align="center">
-        <p style="box-sizing: border-box;display: inline-block;font-family:'Open Sans',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #00000; none;font-size: 30px;"><span style="display:block;padding:10px 20px;line-height:120%;"><strong>{##Verify Email##}</strong></span></p>
+        <a href="{link}" target="_blank" style="box-sizing: border-box;display: inline-block;font-family:'Open Sans',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #000000; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
+        <span style="display:block;padding:10px 20px;line-height:120%;"><strong>Verificar Email</strong></span>
+        </a>
         </div>
         </td>
         </tr>
@@ -357,7 +361,7 @@ def send_email_presenter(event, context):
         </html>
         """
 
-        message = message.format(name=name, code=code)
+        message = message.format(name=name, link=link)
 
         event["response"]["emailMessage"] = message
         event["response"]["emailSubject"] = 'Confirme seu cadastro - Knowly'
@@ -645,7 +649,9 @@ def send_email_presenter(event, context):
         <tr>
         <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:15px;font-family:'Open Sans',sans-serif;" align="left">
         <div align="center">
-        <p style="box-sizing: border-box;display: inline-block;font-family:'Open Sans',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #00000; none;font-size: 30px;"><span style="display:block;padding:10px 20px;line-height:120%;"><strong>{##Verify Email##}</strong></span></p>
+        <a href="{link}" target="_blank" style="box-sizing: border-box;display: inline-block;font-family:'Open Sans',sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #000000; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; width:auto; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;font-size: 14px;">
+        <span style="display:block;padding:10px 20px;line-height:120%;"><strong>Verificar Email</strong></span>
+        </a>
         </div>
         </td>
         </tr>
@@ -716,7 +722,7 @@ def send_email_presenter(event, context):
         </html>
         """
 
-        message = message.format(name=name, code=code)
+        message = message.format(name=name, link=link)
 
         event["response"]["emailMessage"] = message
         event["response"]["emailSubject"] = 'Criar nova senha - Knowly'
